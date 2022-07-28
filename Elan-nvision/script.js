52ba3d468b7ea9d63dd84a72f81c9f01a5fb6b40
Frank980 <105901796+Frank980@users.noreply.github.com>
2022-07-29 03:48:49 +0530

    Delete Elan-nvision directory

--- Elan-nvision/script.js
+++
@@ -1,8 +0,0 @@
-burger = document.querySelector('.burger');
-navbar = document.querySelector('#N');
-ul = document.querySelector("#UL");
-
-burger.addEventListener('click', ()=>{
-    navbar.classList.toggle('h-nav');
-    ul.classList.toggle('v-class');
-})
